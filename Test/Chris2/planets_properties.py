d91590adda070cfc929be46b9fbbc522e3b465ab
Chris Ormel <cwosci@gmail.com>
2024-01-05 17:48:47 +0800

    I thoroughly changed the DISK class. Now part of the physics module (disk = physics.DISK(...)). Code is running, but still work in progress... I dont like how certain things are implemented

--- Test/Chris2/planets_properties.py
+++ Test/Chris2/planets_properties.py
@@ -1,41 +1,60 @@
 import numpy as np
 import cgs
 import functions as f
-import core
+## import core# disk should go
+import physics
+import disk_properties as dp
 
-def planet_migration(gas,planetLoc,planetMass,time,rhopl):
+def planet_migration (gas,planetLoc,planetMass,time,rhopl):
     #aerodynamic drag migration
     out = gas.get_key_disk_properties (planetLoc, time)
-    disk = core.DISK (*out, planetLoc, time)
+
+    mcp = dp.Mcp_t(time)
+
+    disk = physics.DISK (*out, planetLoc, time, mcp) #pro
     disk.add_auxiliary ()
-    disk.user_difined ()
+    disk.add_uservar (dp.user_add_var())    #variables
 
-    eta=disk.eta
-    v_K=disk.vK
-    v_th=disk.vth
-    lmfp=disk.lmfp
-    rho_g=disk.rhog
-    Omega_K=disk.OmegaK
-    dotMg=disk.dotMg
-    Mcp=disk.Mcp
-    mg=disk.mu*cgs.mp
-    Sigmag=disk.sigmaG
-    cs=disk.cs
+    #disk = core.DISK (*out, planetLoc, time)
+    #disk.add_auxiliary ()
+    #disk.user_difined ()
+
+    #CWO: only add what you need!
+    #TBR
+    if False:
+        eta=disk.eta
+        v_K=disk.vK
+        v_th=disk.vth
+        lmfp=disk.lmfp
+        rho_g=disk.rhog
+        Omega_K=disk.OmegaK
+        dotMg=disk.dotMg
+        Mcp=disk.Mcp
+        Sigmag=disk.sigmaG
+        cs=disk.cs
+
+    mg = disk.mu*cgs.mp
+    dotMg = dp.dot_Mg(planetLoc)
     
-    Rpl=(planetMass/(4/3*np.pi*rhopl))**(1/3)
+    ## CWO: Stokes number for planetesimals/planets a bit weird...
+    #for the moment, I put it infinite
+    if False:
+        Rpl=(planetMass/(4/3*np.pi*rhopl))**(1/3)
+        Stpl,vd=f.St_iterate(eta,v_K,v_th,lmfp,rho_g,Omega_K,Rpl)
 
-    Stpl,vd=f.St_iterate(eta,v_K,v_th,lmfp,rho_g,Omega_K,Rpl)
+    vd = 0
 
     #Type I migration
-    qr=-0.14493*dotMg*cgs.gC*Mcp*(-0.206349*planetLoc**(5.5)+planetLoc**4.5*disk.rout)/disk.alpha/planetLoc**(8.5)/disk.rout/np.sqrt(cgs.kB*(dotMg*cgs.gC*Mcp/planetLoc**3/cgs.sigmaSB)**(1/4)/mg) #pressure gradient
+    ## CWO: too messy and too long...
+    qr = -0.14493*dotMg*cgs.gC*mcp*(-0.206349*planetLoc**(5.5)+planetLoc**4.5*disk.rout)/disk.alpha/planetLoc**(8.5)/disk.rout/np.sqrt(cgs.kB*(dotMg*cgs.gC*mcp/planetLoc**3/cgs.sigmaSB)**(1/4)/mg) #pressure gradient
 
-    CI=0.1
-    bt1=CI*(2.7+1.1*qr)   #a constant Ogihara 2014
-    vt1=bt1*(planetMass/Mcp)*(Sigmag*planetLoc**2/Mcp)*(v_K/cs)**2*v_K
+    CI = 0.1
+    bt1 = CI*(2.7+1.1*qr)   #a constant Ogihara 2014
+    vt1 = bt1*(planetMass/mcp)*(disk.sigmaG*planetLoc**2/mcp)*(disk.vK/disk.cs)**2 *disk.vK
 
     v_mig=vt1+vd
-
     return v_mig
+
 
 class Planets(object):
     """
--- main/functions.py
+++ main/functions.py
@@ -113,7 +113,6 @@ def get_stokes_number(disk,t,sPLmtot,rhoint):
     mphy = sPLmtot
     Rd=(mphy/(rhoint*4/3*np.pi))**(1/3)
 
-
     eta=disk.eta
     v_K=disk.vK
     v_th=disk.vth
@@ -191,7 +190,9 @@ def epsilon_PA (system,PlanetsLoc,PlanetsMass,cross_p):
     P_eff=1/np.sqrt((0.32*np.sqrt(mus*delv_o_vK/ St/ eta**2))**(-2)+(0.39*mus/ eta/hp)**(-2)) #Liu & Ormel 2018
     return P_eff
 
+
 def M_critical(system,PlanetsLoc,cross_p):
+    ## CWO: this needs to be re-thought
 
     out = system.gas.get_key_disk_properties (PlanetsLoc, system.time)
     disk = core.DISK (*out, PlanetsLoc, system.time)
